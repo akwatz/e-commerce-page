f12a9f2b7f2148ed179765e2e08fce199bd53287
Aman Wats <aman.wats@outlook.com>
2021-01-28 22:57:00 +0530

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-function App() {
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
   return (
     <>
-    <h1>Welcome to the Pro Shop</h1>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <h1>Welcome to the Pro Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
