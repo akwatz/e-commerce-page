307dce131d6ce9130499e853e9a3e76c1f4875bd
Aman Wats <aman.wats@outlook.com>
2021-02-16 20:59:57 +0530

    connected mongo db with the app

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import products from './data/products.js';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +20,13 @@ app.get('/api/products/:id', (req, resp) => {
   resp.json(product);
 });
 
-app.listen(5000, console.log('app is running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `app is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+connectDB();
